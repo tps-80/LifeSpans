6cc940116cfeff214f3791da8bb9032a160bef48
Thomas Sorensen <thomaspsorensen@gmail.com>
2016-02-08 12:31:58 -0800

    added home route

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/home', function (req, res) {
-  res.send('This is my home route.');
+  // res.send('This is my home route.');
+  res.redirect('/index.html')
 });
 
 app.listen(process.env.PORT || 3000, function () {
